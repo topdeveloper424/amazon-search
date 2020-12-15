974e75f50d29629dd3af9152a21b94b13e259b05
topdeveloper424 <leebin424224@gmail.com>
2020-12-14 22:55:11 -0500

    added login

--- back-end/config.js
+++ back-end/config.js
@@ -1,7 +1,7 @@
 module.exports  = {
     // Remote monogodb url
-//    mongoURL: "mongodb+srv://Serverusr1:J6ZqMqrjJGVSQGqq@cluster0.gbgc3.azure.mongodb.net/amazon_db?retryWrites=true&w=majority",
-    mongoURL: 'mongodb://localhost:27017/amazon_db',
+    mongoURL: "mongodb+srv://Serverusr1:J6ZqMqrjJGVSQGqq@cluster0.gbgc3.azure.mongodb.net/amazon_db?retryWrites=true&w=majority",
+//    mongoURL: 'mongodb://localhost:27017/amazon_db',
  
     // Request limit number
     batchNumber: 500,
--- back-end/controllers/authentication.js
+++ back-end/controllers/authentication.js
@@ -5,13 +5,17 @@
 //= =======================================
 exports.login = function (req, res, next) {
     console.log('connecting...');
-    let  {Username, Password } = req.body;
-    if( Username !== undefined && Password !== undefined ) {
-        if(Username === "admin" && Password === "admin"){
-            res.status(200).send({status:"success"});
+    let username  = req.body.username;
+    let password  = req.body.password;
+    console.log("password",password)
+    if(typeof username !== undefined && typeof password !== undefined ) {
+        if(username === "admin" && password === "admin"){
+            res.end(JSON.stringify({status:"success"}));
+        }else{
+            res.end(JSON.stringify({status:"failed"}));
         }
     }else{
-        console.log('Undefined Username or Password')
-        res.status(401).send('No parameters');
+        res.end(JSON.stringify({status:"error"}));
     }
+
 };
